0acbd1ffc7b78a1fec6072cc0a5aaf14c1a8e8e7
Abhishek Shah <abhishekshah3@gmail.com>
2021-08-29 12:25:47 +0530

    typing 'quit' or 'exit' quits the REPL

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 mod math;
-use math::tokenizer::Tokenizer;
 use math::token::Token;
+use math::tokenizer::Tokenizer;
 fn main() {
     let quit = "quitexit";
     let mut buf = String::new();
@@ -10,7 +10,8 @@ fn main() {
         buf.clear();
         println!("Enter an expression to evaluate");
         stdin.read_line(&mut buf);
-        if quit.contains(&buf[..]) {
+        remove_crlf(&mut buf);
+        if quit.contains(&buf) {
             break;
             println!("Exiting ...");
         }
@@ -18,6 +19,15 @@ fn main() {
         for t in tkn {
             print!("{:?} ", t);
         }
-        println!("");
+        println!("\n****************************");
+    }
+}
+
+fn remove_crlf(s: &mut String) {
+    if s.ends_with('\n') {
+        s.pop();
+        if s.ends_with('\r') {
+            s.pop();
+        }
     }
 }
